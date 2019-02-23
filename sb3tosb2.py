85d80cf2e7979e197366c8e70f295e9a7f8758b1
RexScratch <rex5554scratch@gmail.com>
2019-02-22 19:10:30 -0600

    Stack overflow prevention added

--- sb3tosb2.py
+++ sb3tosb2.py
@@ -1,4 +1,5 @@
 import sys, json, zipfile
+sys.setrecursionlimit(4100)
 
 def printWarning(message):
     print("WARNING: " + message)
@@ -1008,23 +1009,33 @@ class ProjectConverter:
                 self.hackedReporterBlockID(value)
 
     def convertBlock(self, block, blocks):
+        self.depth += 1 # Bad solution to track depth
+
         opcode = block['opcode']
-        self.blockID += 1
-        try:
-            return self.argmapper.mapArgs(opcode, block, blocks)
-        except:
-            if len(block['inputs']) == 0 and len(block['fields']) == 1 and block['shadow']: # Menu opcodes and shadows
-                self.blockID -= 1
-                return self.fieldVal(list(block['fields'].items())[0][0], block)
-            else:
-                self.generateWarning("Incompatible opcode '{}'".format(opcode))
-                
-                output = [opcode]
-                for i in block['inputs']:
-                    output.append(self.inputVal(i, block, blocks))
-                for f in block['fields']:
-                    output.append(self.fieldVal(f, block))
+        if self.depth < 1000:
+            self.blockID += 1
+            try:
+                output = self.argmapper.mapArgs(opcode, block, blocks)
+                self.depth -= 1
                 return output
+            except:
+                if len(block['inputs']) == 0 and len(block['fields']) == 1 and block['shadow']: # Menu opcodes and shadows
+                    self.blockID -= 1
+                    output = self.fieldVal(list(block['fields'].items())[0][0], block)
+                    self.depth -= 1
+                    return output
+                else:
+                    output = [opcode]
+                    for i in block['inputs']:
+                        output.append(self.inputVal(i, block, blocks))
+                    for f in block['fields']:
+                        output.append(self.fieldVal(f, block))
+                    self.generateWarning("Incompatible opcode '{}'".format(opcode))
+
+                    self.depth -= 1
+                    return output
+        else: # Stack Overflow
+            return ''
 
     def inputVal(self, value, block, blocks):
 
@@ -1277,6 +1288,7 @@ class ProjectConverter:
                 if y % 1 == 0:
                     y = int(y)
                 
+                self.depth = 0
                 scripts.append([x, y, self.convertSubstack(key, blocks)])
                 self.scriptCount += 1
         
